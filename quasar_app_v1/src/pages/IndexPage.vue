09fd24455f2f70b7d3133fef7ba8b18bba8a9500
phantom <akashmelkeri23@gmail.com>
2022-12-03 18:12:54 +0530

    index start

--- quasar_app_v1/src/pages/IndexPage.vue
+++ quasar_app_v1/src/pages/IndexPage.vue
@@ -1,6 +1,12 @@
 <template>
   <q-page class="flex justify-center">
-    <div class="tw-container">ok</div>
+    <div class="tw-container flex justify-center">
+      <div class="column items-center bg-red">
+        <h1 class="text-h3 text-bold">MyDevTools</h1>
+        <p>THE LARGEST AI TOOLS DIRECTORY, UPDATED DAILY.</p>
+      </div>
+      
+    </div>
   </q-page>
 </template>
 
